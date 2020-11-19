547f04b51324a5884a3a1fc6089e8af95b8cb49c
Sem van der Hoeven <semmer99@gmail.com>
2020-11-19 14:59:20 +0100

    [ADD] added post request method

--- PhilipsHueApk/app/src/main/java/com/example/philipshueapk/HttpHandler.java
+++ PhilipsHueApk/app/src/main/java/com/example/philipshueapk/HttpHandler.java
@@ -1,13 +1,18 @@
 package com.example.philipshueapk;
 
+import java.io.IOException;
 import java.util.Objects;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public enum HttpHandler {
     INSTANCE;
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private OkHttpClient client;
     private String username = "newDeveloper";
@@ -28,6 +33,24 @@ public enum HttpHandler {
                 .build();
 
         try (Response response = client.newCall(request).execute()){
+            return Objects.requireNonNull(response.body()).string();
+        }
+    }
+
+    /**
+     * sends a post request to the API and returns the response
+     * @param url the url of the API to use
+     * @param jsonMsg the json message to post
+     * @return the response of the API
+     * @throws IOException when there was an error with parsing the json or with the response
+     */
+    public String postRequest(String url, String jsonMsg) throws IOException {
+        RequestBody body = RequestBody.create(jsonMsg, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
             return Objects.requireNonNull(response.body()).string();
         }
     }
