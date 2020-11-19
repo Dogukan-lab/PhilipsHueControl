94a9fa2b1778b7028eaf90acf3f2180bab357f5f
Sem van der Hoeven <semmer99@gmail.com>
2020-11-19 14:55:57 +0100

    [ADD] added create request method

--- PhilipsHueApk/app/src/main/java/com/example/philipshueapk/HttpHandler.java
+++ PhilipsHueApk/app/src/main/java/com/example/philipshueapk/HttpHandler.java
@@ -1,6 +1,10 @@
 package com.example.philipshueapk;
 
+import java.util.Objects;
+
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public enum HttpHandler {
     INSTANCE;
@@ -10,6 +14,22 @@ public enum HttpHandler {
 
     public void initHttpClient() {
         client = new OkHttpClient();
+    }
+
+    /**
+     * sends a request to the API and returns the response
+     * @param url the url to send to the api
+     * @return a String with the response
+     * @throws Exception if there was an error with the response
+     */
+    public String createRequest(String url) throws Exception{
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()){
+            return Objects.requireNonNull(response.body()).string();
+        }
     }
 
     public OkHttpClient getClient() {
