8e535569a2f78a1c3d366c5fbee5d0d44db62039
Sem van der Hoeven <semmer99@gmail.com>
2020-11-19 14:46:03 +0100

    [EDIT] made sure fragment gets added to main activity by fragment manager and not in the xml

--- PhilipsHueApk/app/src/main/java/com/example/philipshueapk/MainActivity.java
+++ PhilipsHueApk/app/src/main/java/com/example/philipshueapk/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.philipshueapk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment fragment = MainFragment.newInstance("test1","test2");
+        fragmentTransaction.add(R.id.mainActivity,fragment);
+        fragmentTransaction.commit();
     }
 }
