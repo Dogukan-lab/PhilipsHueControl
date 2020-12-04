0d3a07e5c7688e4d1bb3b99d8195a531356aa446
Dogukan <dogukan-akyazi@hotmail.com>
2020-12-04 14:08:58 +0100

    [ADDITION] Added tests classes for: lampState and LampConfig

--- PhilipsHueApk/app/src/test/java/com/example/philipshueapk/lamp/LampStateTest.java
+++ PhilipsHueApk/app/src/test/java/com/example/philipshueapk/lamp/LampStateTest.java
@@ -1,95 +1,115 @@
 package com.example.philipshueapk.lamp;
 
-import junit.framework.TestCase;
+import android.graphics.Color;
+import android.util.Log;
 
-import org.junit.*;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LampStateTest extends TestCase {
 
     private LampState lampState;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         super.setUp();
         lampState = new LampState();
     }
 
     @AfterEach
-    public void tearDown() throws Exception {
+    public void tearDown() {
+        lampState = null;
     }
 
+    @Test
     public void testGetOn() {
+        lampState.setOn(true);
+
+        assertTrue(lampState.getOn());
     }
 
-    public void testSetOn() {
-    }
-
+    @Test
     public void testGetBri() {
+        lampState.setBri(200);
+
+        assertEquals(200, lampState.getBri().intValue());
     }
 
-    public void testSetBri() {
-    }
-
+    @Test
     public void testGetHue() {
+        lampState.setHue(4444);
+
+        assertEquals(4444, lampState.getHue().intValue());
     }
 
-    public void testSetHue() {
-    }
-
+    @Test
     public void testGetSat() {
+        lampState.setSat(254);
+
+        assertEquals(254, lampState.getSat().intValue());
     }
 
-    public void testSetSat() {
-    }
-
+    @Test
     public void testGetEffect() {
+        lampState.setEffect("Disco");
+
+        assertEquals("Disco", lampState.getEffect());
     }
 
-    public void testSetEffect() {
-    }
-
+    @Test
     public void testGetXy() {
+        List<Double> xy = new ArrayList<>();
+        xy.add(0.345);
+        xy.add(0.3568);
+        lampState.setXy(xy);
+
+        assertEquals(0.345, lampState.getXy().get(0));
+        assertEquals(0.3568,lampState.getXy().get(1));
     }
 
-    public void testSetXy() {
-    }
-
+    @Test
     public void testGetCt() {
+        lampState.setCt(40);
+
+        assertEquals(40, lampState.getCt().intValue());
     }
 
-    public void testSetCt() {
-    }
-
+    @Test
     public void testGetAlert() {
+        String alert = "Not connected";
+        lampState.setAlert(alert);
+
+        assertEquals(alert, lampState.getAlert());
     }
 
-    public void testSetAlert() {
-    }
-
+    @Test
     public void testGetColormode() {
+        lampState.setColormode("hs");
+
+        assertEquals("hs", lampState.getColormode());
     }
 
-    public void testSetColormode() {
-    }
-
+    @Test
     public void testGetMode() {
+        lampState.setMode("homeautomation");
+
+        assertEquals("homeautomation", lampState.getMode());
     }
 
-    public void testSetMode() {
-    }
-
+    @Test
     public void testGetReachable() {
+        lampState.setReachable(true);
+
+        assertTrue(lampState.getReachable());
     }
 
-    public void testSetReachable() {
-    }
-
+    @Test
     public void testGetAdditionalProperties() {
-    }
+        lampState.setAdditionalProperty("lampOne", 20);
 
-    public void testSetAdditionalProperty() {
-    }
-
-    public void testCalculateRGBColor() {
+        assertEquals(20, lampState.getAdditionalProperties().get("lampOne"));
     }
 }
