3f74dbf998c16838f4bfe0f1a6e77f58589017a1
Dogukan <dogukan-akyazi@hotmail.com>
2020-12-04 13:27:04 +0100

    [ADDITION] fixed text size and added a test class

---
+++ PhilipsHueApk/app/src/test/java/com/example/philipshueapk/lamp/LampStateTest.java
@@ -0,0 +1,95 @@
+package com.example.philipshueapk.lamp;
+
+import junit.framework.TestCase;
+
+import org.junit.*;
+
+public class LampStateTest extends TestCase {
+
+    private LampState lampState;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        lampState = new LampState();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetOn() {
+    }
+
+    public void testSetOn() {
+    }
+
+    public void testGetBri() {
+    }
+
+    public void testSetBri() {
+    }
+
+    public void testGetHue() {
+    }
+
+    public void testSetHue() {
+    }
+
+    public void testGetSat() {
+    }
+
+    public void testSetSat() {
+    }
+
+    public void testGetEffect() {
+    }
+
+    public void testSetEffect() {
+    }
+
+    public void testGetXy() {
+    }
+
+    public void testSetXy() {
+    }
+
+    public void testGetCt() {
+    }
+
+    public void testSetCt() {
+    }
+
+    public void testGetAlert() {
+    }
+
+    public void testSetAlert() {
+    }
+
+    public void testGetColormode() {
+    }
+
+    public void testSetColormode() {
+    }
+
+    public void testGetMode() {
+    }
+
+    public void testSetMode() {
+    }
+
+    public void testGetReachable() {
+    }
+
+    public void testSetReachable() {
+    }
+
+    public void testGetAdditionalProperties() {
+    }
+
+    public void testSetAdditionalProperty() {
+    }
+
+    public void testCalculateRGBColor() {
+    }
+}
